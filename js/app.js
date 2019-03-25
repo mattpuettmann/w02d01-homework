6e2915b908fdc34f2f960ebeb066debe7a7e4a76
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-03-25 16:08:34 -0600

    Commit 1 - datatypes

---
+++ js/app.js
@@ -0,0 +1,56 @@
+console.log("Hello!");
+
+//1. Datatypes
+// A light switch that can be either on or off.
+//		Boolean
+//		"lightSwitchOn === true"
+// A user's email address.
+//		String
+//		"matthewpuettmann@gmail.com"
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//		Array
+//		const spaceship = [
+//		hull,
+//		laser blasters,
+//		tractor beam
+//		]
+// A list of student names from our class.
+//		Array
+//		const students = ["Matt", "Steve", "Billy Joe", "Jim Bob"]
+// A list of student names from our class, each with a location.
+//		Object
+//		const studentsNames = {
+//			{name: 'Matt',
+//			 location : 'Denver'
+//			},
+//			{name: 'Bill',
+//			 location: 'Denver'
+//			}
+//		}
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+//		Object
+//		const studentsNames = {
+//			{name: 'Matt',
+//			 location : 'Denver',
+//			 favShows: ['IASIP', 'GoT', 'BB']
+//			},
+//			{name: 'Bill',
+//			 location: 'Denver',
+//			 favShows: ['Curb', 'South Park', 'Shark Tank']
+//			}
+//		}		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
